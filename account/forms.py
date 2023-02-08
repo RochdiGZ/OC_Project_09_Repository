e0c8a849c8790100371de3f64710f392384c8468
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2023-02-08 01:18:14 +0100

    Second commit

--- account/forms.py
+++ account/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label="Nom d'utilisateur")
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label="Mot de passe")
+    username = forms.CharField(max_length=63, label="Nom d'utilisateur : ",
+                               widget=forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"}))
+    password = forms.CharField(max_length=63, label="Mot de passe : ",
+                               widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"}))
--- account/models.py
+++ account/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 
 class User(AbstractUser):
-    profile_photo = models.ImageField(
-        blank=True,
-        upload_to="",
-        verbose_name="Photo de profil")
+    profile_photo = models.ImageField(blank=True, verbose_name="Photo de profil", upload_to="")
+
+
+"""
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=63)
+    password = models.CharField(max_length=63)
+    profile_photo = models.ImageField(blank=True, verbose_name="Photo de profil", upload_to="")
+"""
